de1176b222ae5f17c1c5635ca5fdf57e788c5947
Marco Zaccaro <marco@marcozaccaro.com>
2024-10-10 07:53:06 +0000

    chore: init `npm` package

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
+
+    {
+        ignores: ['**/dist/', '**/testdata/', 'example/'],
+    },
+);
